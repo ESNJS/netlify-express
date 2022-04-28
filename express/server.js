d893c0f5f73ccc5677503d7f99963213abb89655
Erik <esnetto@gmail.com>
2022-04-28 18:17:19 -0300

    lalaa

--- express/server.js
+++ express/server.js
@@ -92,9 +92,10 @@ router.get('/',  (req, res) => {
   )
 });
 
+router.get('/another', (req, res) => {console.log(req)});
+
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
